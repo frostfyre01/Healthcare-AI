afb468ebc3b3178acef48b082c632ef163172b4b
isalee319818 <81532403+isalee319818@users.noreply.github.com>
2024-08-04 13:53:02 -0500

    add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" width="256">
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/mirna">
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <!-- Add more items as needed -->
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
+      <!-- Main content here -->
       <router-view />
     </v-main>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>My Application</v-toolbar-title>
+      <v-spacer />
+      <!-- Add additional actions or components here -->
+    </v-app-bar>
   </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  data: () => ({
+    drawer: false
+  })
+}
 </script>
+
+<style>
+/* Add any custom styles for your layout here */
+</style>
---
+++ src/pages/MiRNA.vue
@@ -0,0 +1,7 @@
+<template>
+    This is MiRNA!
+  </template>
+  
+  <script setup>
+    //
+  </script>
