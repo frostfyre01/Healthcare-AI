65c2e941f7eb810b883d0fc9829ef21d105c09a8
isalee319818 <81532403+isalee319818@users.noreply.github.com>
2024-08-04 14:01:25 -0500

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" width="256">
-      <v-list>
-        <v-list-item link to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/mirna">
-          <v-list-item-icon>
-            <v-icon>mdi-information</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>About</v-list-item-title>
-        </v-list-item>
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-home" title="Home" link to="/"></v-list-item>
+        <v-list-item prepend-icon="mdi-home" title="MiRNA" link to="/mirna"></v-list-item>
         <!-- Add more items as needed -->
       </v-list>
     </v-navigation-drawer>
 
     <v-main>
-      <!-- Main content here -->
       <router-view />
     </v-main>
 
@@ -27,13 +16,20 @@
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>My Application</v-toolbar-title>
       <v-spacer />
-      <!-- Add additional actions or components here -->
     </v-app-bar>
+
+    <!-- Footer -->
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
+import AppFooter from './components/AppFooter.vue';
+
 export default {
+  components: {
+    AppFooter,
+  },
   data: () => ({
     drawer: false
   })
