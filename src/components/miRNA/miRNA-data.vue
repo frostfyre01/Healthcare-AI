cb4c3d46b278382815ad402b8c15d01e94288846
isalee319818 <81532403+isalee319818@users.noreply.github.com>
2024-09-29 23:52:54 -0500

    content

--- src/components/miRNA/miRNA-data.vue
+++ src/components/miRNA/miRNA-data.vue
@@ -1,58 +1,43 @@
 <template>
    <div class="container">
+        <v-alert type="info" border="left" prominent>
+            Please use the following link to access the Jupyter Notebook:
+            <v-btn text @click="openLink" class="ml-2">Open Notebook</v-btn>
+        </v-alert>
         <div class="section">
-            <h2>Overview</h2>
-            <p>This summer, I participated in a high school stock trading competition, which gave me the chance to explore the world of stock trading from a fresh perspective. By leveraging my data analytics skills, I implemented several trading strategies, allowing me to delve deeper into the nuances of rule-based trading. This experience not only improved my analytical skills but also offered valuable insights into how systematic approaches can impact trading outcomes. I applied my newly developed strategies in the competition and won first place, earning a $600 award out of over 300 participants.</p>
-            <p>In light of this experience, I'm sharing the detailed analysis of the data-driven approach I employed, the specific trading rules I implemented, and the outcomes achieved. I aim to offer insights and practical guidance for those interested in rule-based trading, while also reflecting on the effectiveness of different trading strategies in actual market conditions.</p>
+            <h2>Data Source</h2>
+            <p>The <b>National Institutes of Health (NIH)</b> offers a vast array of research studies, clinical trials, and reviews on biomarkers, including microRNAs.</p>
+            <p>By searching relevant terms on <a href="https://www.ncbi.nlm.nih.gov/" target="_blank">NIH.gov</a> we can find many articles that detail the methods, outcomes, and importance of using microRNAs in blood tests for cancer diagnosis. Here are a few examples:</p>
+            <ul style="margin-left: 30px;">
+                <li><a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE137140" target="_blank">Lung Cancer Dataset</a></li>
+                <li><a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE164174" target="_blank">Gastric Cancer Dataset</a></li>
+                <li><a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE122497" target="_blank">Esophageal Dataset</a></li>
+                <li><a href="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE113486" target="_blank">Bladder Cancer Dataset</a></li>
+            </ul>
         </div>
 
         <div class="section">
-            <h2>What is Rule-based Trading</h2>
-            <p>Stock trading is challenging for beginners due to the complexities of market analysis, emotional decision-making, and risk management. Understanding market trends, choosing the right stocks, and developing effective strategies can be overwhelming. Additionally, the emotional ups and downs and potential for losses can make trading stressful and difficult.</p>
-            <p>Rule-based methods can assist in handling various trading challenges. Rule-based trading involves following predefined criteria for buying and selling assets. Traders set specific rules based on indicators or data, which determine entry and exit points. This approach reduces emotional influence and ensures consistent, systematic decision-making, leading to more predictable trading results.</p>
-            <p>Here are a few benefits of rule-based trading:</p>
-            <div style="margin-left: 30px;">
-                <ul>
-                    <li><b>Consistency:</b> <i>Adheres to predefined criteria, reducing emotional decision-making, avoiding impulsive trades.</i></li>
-                    <li><b>Backtesting:</b> <i>Allows for the evaluation of strategies using historical data, enabling us to estimate the outcomes of trading under similar market conditions.</i></li>
-                    <li><b>Efficiency:</b> <i>Automates trading decisions, saving time and effort.</i></li>
-                </ul>
-            </div>
-
+            <h2>Data Structure</h2>
+            <p>miRNA readings consist of fields like miRNA names, expression levels, clinical data, and patient identifiers. Here is a sample of the data for lung cancer.</p>
         </div>
+        <img src="@/assets/images/miRNA/mirna-data.png" />
 
         <div class="section">
-            <h2>Trading Expectancy</h2>
-            <p>In simple terms, when we analyze a trading system using a systematic approach, we focus on ensuring that the trading expectancy is positive. <b>Trading Expectancy</b> is calculated as:</p>
-            <img src="@/assets/images/trading/expectancy.png">
-            <p>Imagine a betting game where you place a $2 bet each time to draw a marble from a bag containing 6 red marbles and 4 black marbles. The rules are as follows:
-                <ul style="margin-left: 30px;">
-                    <li>If you draw a red marble, you win $3.</li>
-                    <li>If you draw a black marble, you lose your $2 bet.</li>
-                </ul>
-            </p>
-            <p>In this game, even though you have a higher probability of winning money, the expected value is negative.</p>
-            <p class="code">Expected Value = (0.6 × 1) + (0.4 × (−2)) = 0.6 − 0.8 = −0.2<br/>
-            The expected value of this betting game is −$0.20. This means that, on average, you would lose 20 cents per bet in the long run.
-            </p>
-            <p><b>Trading Expectancy</b> helps traders evaluate strategies by quantifying their performance. A positive expectancy indicates that the strategy is likely profitable over time, while a negative expectancy suggests potential losses and should generally be avoided.</p>
+            <h2>Data Processing</h2>
+            <p>Before creating a model, several data processing steps should be performed, including:</p>
+            <ul style="margin-left: 30px">
+                <li><b>Data Cleaning:</b> Remove duplicates, handle missing values, and correct errors.</li>
+                <li><b>Data Transformation:</b> Normalize or standardize data to ensure consistency.</li>
+                <li><b>Encoding Categorical Variables:</b> Convert categorical data into numerical format if necessary.</li>
+            </ul>
         </div>
+        <img src="@/assets/images/miRNA/mirna-class.png" />
 
         <div class="section">
-            <h2>Design Flow</h2>
-            <p>When designing a trading strategy, consider the following key steps:</p>
-            <p>
-                <ul style="margin-left: 30px;">
-                    <li><b>Define Goals:</b> Set clear objectives, including target returns, risk tolerance, and time horizons.</li>
-                    <li><b>Develop Trading Logic:</b> Choose indicators and rules for trade entries and exits.</li>
-                    <li><b>Backtest Using Historical Data:</b> Test trading logic on historical data to create a trade list with entry and exit points.</li>
-                    <li><b>Evaluate Performance:</b> Analyze results using metrics like profit factor, win/loss ratio, and drawdowns.</li>
-                    <li><b>Adjust Parameters:</b> Refine strategy based on performance and re-test with adjusted parameters.</li>
-                    <li><b>Implement and Monitor:</b> Apply the strategy in live trading and regularly review its performance, adjusting as needed.</li>
-                </ul>
-            </p>            
-            <img src="@/assets/images/trading/design-flow.png">
+            <h2>Feature Selection</h2>
+            <p>Identify and select relevant features that contribute to the model.</p>
         </div>
+        <img src="@/assets/images/miRNA/mirna-hue.png" />
 
         <div class="section">
             <v-btn color="primary" @click="handleButtonClick">Learn More</v-btn>
@@ -64,8 +49,11 @@
 export default {
   methods: {
     handleButtonClick() {
-        this.$emit('change-tab', 'data');
-    }
+        this.$emit('change-tab', 'model');
+    },
+    openLink() {
+      window.open('https://deepnote.com/app/healthcare-ai-web/miRNA-cancer-detection-8bcf6c22-7a5a-48dc-85d6-e976abbf3aaa', '_blank');
+    },
   }
 }
 </script>
